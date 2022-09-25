755b9f0ed28b1bf254c2b184cc5202dc580eb8fd
Mark Potocki <mepotock@asu.edu>
2022-09-25 16:02:24 -0500

    update post profile

--- lambdas/post_profile.py
+++ lambdas/post_profile.py
@@ -1,39 +1,20 @@
-import os
 import boto3
 dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
 table = dynamodb.Table('UserProfile')
 
 def lambda_handler(event, context):
     try: 
-        new_profile = event
+        new_profile = event['body']
         new_profile['Username'] = event['requestContext']['identity']['cognitoIdentityId']
 
         return {
-                'statusCode': 200,
-                'headers':{'Content-Type': 'application/json'},
-                'body': ['Item']
+                'statusCode': 204
             } 
     except Exception as e:
         print(e)
         return {'statusCode': 400} 
 
-def post_profile(username: str) -> dict:
-    profile = table.put_item(
-        Item = {
-            'Username': username
-        }
-    )
-    return profile     
-
-def test_update_profile():
-    test_event = {
-        'requestContext': {
-            'identity': {
-                'cognitoIdentityId': '0893be13-3d6c-489e-9029-63e4b28945cb'
-            }
-        }
-    }
-
-    print(lambda_handler(test_event, {}))
-
-test_update_profile()
+def post_profile(new_profile: dict) -> None:
+    table.put_item(
+        Item = new_profile
+    )   
